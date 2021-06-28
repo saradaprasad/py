00e5eeca636bcac50fe59a7913eccb14ff5c0225
prasad <prasadsarada7@gmail.com>
2021-06-28 21:36:05 +0530

    addedd few more coding exercies

---
+++ py_basics/operators_mathematical.py
@@ -0,0 +1,13 @@
+#mathmatical operators
+x = 5
+y = 9
+print('Addition :', x+y)
+print('Substraction :', x-y)
+print('Division with floating-point :',x/y)
+print('Integer Traubcate Divisin :',x//y)
+print('Exponenxt :',x**y)
+print('Multiplication :',x*y)
+print('Modulus :',x%y)
+
+#precedence
+print(2+3*8)
